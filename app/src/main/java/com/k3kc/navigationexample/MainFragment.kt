1f4c0dc66f74d075f4cececd35757c042bc36d09
x913 <x913x913@gmail.com>
2020-05-22 20:23:56 +0500

    Initial commit

---
+++ app/src/main/java/com/k3kc/navigationexample/MainFragment.kt
@@ -0,0 +1,38 @@
+package com.k3kc.navigationexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment(), View.OnClickListener {
+
+    var navController: NavController? = null
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        view.findViewById<Button>(R.id.view_transactions_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.send_money_btn).setOnClickListener(this)
+        view.findViewById<Button>(R.id.view_balance_btn).setOnClickListener(this)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+
+}
